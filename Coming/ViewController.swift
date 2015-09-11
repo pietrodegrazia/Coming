8d724f5c705de328f6e17ba90f69febec918ee04
pietrodegrazia <pietrodegrazia@hotmail.com>
2015-09-11 04:41:12 -0300

    Initial Commit

---
+++ Coming/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Coming
+//
+//  Created by Pietro Degrazia on 9/11/15.
+//  Copyright (c) 2015 DarkShine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
